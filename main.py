d717af8ef5abb848628bea17bed4823f0c67ae83
DeathMaroko <asikkoko@gmail.com>
2024-02-29 17:17:52 +0100

    Add a menu bar

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-     QLineEdit, QPushButton
-
+     QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtGui import QAction
 import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+
+app = QApplication(sys.argv)
+student_management_system = MainWindow()
+student_management_system.show()
+sys.exit(app.exec())
